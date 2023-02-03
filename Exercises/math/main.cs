0995acec3d301cdfdce0231ea412a7602493160d
Ella Lassen <202104770@post.au.dk>
2023-02-03 14:21:19 +0100

    Exercise math

---
+++ Exercises/math/main.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+using static System.Math;
+
+class math{
+	static void Main(){
+		double sqrt2 = System.Math.Sqrt(2.0);
+		System.Console.Write($"sqrt(2) = {sqrt2}\n");
+		
+		System.Console.Write($"sqrt(2)*sqrt(2) = {sqrt2*sqrt2}\n");
+		
+		double epi = System.Math.Exp(System.Math.PI);
+		System.Console.Write($"e^pi = {epi}\n");
+
+		double pie = Pow(PI, E);
+		Write($"pi^e = {pie}\n");
+	}
+}
+
